6a833a504891396c0ebfd153dbc891b08c7fd6ad
HasanHazazn <156054509+HasanHazazn@users.noreply.github.com>
2024-01-13 06:07:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
+const audioYes1 = new Audio('audio-yes1.mp3');  // Path to the first 'yes' audio file
+const audioYes2 = new Audio('audio-yes2.mp3');  // Path to the second 'yes' audio file
+const audioNo = new Audio('audio-no.mp3');      // Path to the 'no' audio file
+
+let currentYesAudio = audioYes1;
+
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "I am the happiest man in the world..hehe";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    question.innerHTML = "I am the happiest man in the world..hehe";
+    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+
+    // Change the audio each time 'Yes' is clicked
+    if (currentYesAudio === audioYes1) {
+        currentYesAudio = audioYes2;
+    } else {
+        currentYesAudio = audioYes1;
+    }
+
+    currentYesAudio.play();
 });
 
 noBtn.addEventListener("mouseover", () => {
-  const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - noBtnRect.width;
-  const maxY = window.innerHeight - noBtnRect.height;
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
 
-  const randomX = Math.floor(Math.random() * maxX);
-  const randomY = Math.floor(Math.random() * maxY);
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+    audioNo.play();
 });
