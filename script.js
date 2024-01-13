ff1c7476855e25aaa10166d746f40a69fa8e1aa6
HasanHazazn <156054509+HasanHazazn@users.noreply.github.com>
2024-01-13 12:52:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,13 +7,25 @@ const noBtn = document.querySelector(".no-btn");
 const audioYes = new Audio('audio-yes.mp3');  // Path to the 'yes' audio file
 const audioNo = new Audio('audio-no.mp3');    // Path to the 'no' audio file
 
-yesBtn.addEventListener("click", () => {
-    question.innerHTML = "I am the happiest man in the world..hehe";
-    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
-    audioYes.play();
-});
+// Check if the user is on a mobile device
+const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
-noBtn.addEventListener("touchstart", (event) => {
+if (isMobile) {
+    // Mobile behavior: move the 'No' button on touch
+    noBtn.addEventListener("touchstart", (event) => {
+        moveNoButton();
+        event.preventDefault(); // Prevent default touch event behavior
+        audioNo.play();
+    });
+} else {
+    // Desktop behavior: move the 'No' button on mouseover
+    noBtn.addEventListener("mouseover", () => {
+        moveNoButton();
+        audioNo.play();
+    });
+}
+
+function moveNoButton() {
     const noBtnRect = noBtn.getBoundingClientRect();
     const maxX = window.innerWidth - noBtnRect.width;
     const maxY = window.innerHeight - noBtnRect.height;
@@ -23,9 +35,10 @@ noBtn.addEventListener("touchstart", (event) => {
 
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
+}
 
-    // Prevent default touch event behavior to avoid issues on some devices
-    event.preventDefault();
-
-    audioNo.play();
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "I am the happiest man in the world..hehe";
+    gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+    audioYes.play();
 });
